4ed440fd99f48c1e39e88407d71f27ebd09340b6
Alex Lepsa <lepsalex@gmail.com>
2024-12-14 12:41:41 -0500

    template dir

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Advent of Code 2024 (C++)
+
+Trying out C++23 with Bazel on Advent of Code 2024!
+
+## Requirements
+- C++23
+- Bazel
+
+## Build
+`bazel build //...`
+
+## Test
+All - `bazel test //...`
+Specific Day - `bazel test main/{DAY}:test`
+
+## Run
+Specific Day - `bazel run main/{DAY}:main -- main/{DAY}/input.txt`
--- lib/input.h
+++ lib/input.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <sstream>
 #include <vector>
+
+using Predicate = std::function<int(int)>;
+
+void ltrim(std::string &str, Predicate const &pred = isspace);
+
+void rtrim(std::string &str, Predicate const &pred = isspace);
+
+void trim(std::string &str, Predicate const &pred = isspace);
 
 inline bool get_input_lines(const std::string_view file_path, std::vector<std::string> &out_vec) {
     std::ifstream input(file_path);
@@ -17,4 +28,33 @@ inline bool get_input_lines(const std::string_view file_path, std::vector<std::s
     }
 
     return true;
+}
+
+inline std::vector<std::string> clean_input(const std::string &input) {
+    std::vector<std::string> result;
+
+    std::stringstream string_stream(input);
+    std::string line;
+
+    while (getline(string_stream, line)) {
+        trim(line);
+        if (!line.empty()) {
+            result.push_back(line);
+        }
+    }
+
+    return result;
+}
+
+inline void ltrim(std::string &str, Predicate const &pred) {
+    str.erase(str.begin(), std::ranges::find_if_not(str, pred));
+}
+
+inline void rtrim(std::string &str, Predicate const &pred) {
+    str.erase((std::find_if_not(str.rbegin(), str.rend(), pred)).base(), str.end());
+}
+
+inline void trim(std::string &str, Predicate const &pred) {
+    ltrim(str, pred);
+    rtrim(str, pred);
 }
