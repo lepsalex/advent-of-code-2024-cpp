5a725008e6fbb6546413f6230f52c5e9e7c6ed8e
Alex Lepsa <lepsalex@gmail.com>
2024-12-14 14:37:13 -0500

    day-02 start

---
+++ main/day-02/solution.cpp
@@ -0,0 +1,19 @@
+#include "solution.h"
+
+#include <istream>
+
+long solution_part_one(const std::vector<std::string> &input) {
+    for (const auto &line: input) {
+        std::print("{}\n", line);
+    }
+
+    return 0;
+}
+
+long solution_part_two(const std::vector<std::string> &input) {
+    for (const auto &line: input) {
+        std::print("{}\n", line);
+    }
+
+    return 0;
+}
---
+++ main/day-02/test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "solution.h"
+#include "lib/input.h"
+
+TEST(SolutionTest, PartOneExample) {
+    const auto input = R"(
+    8999
+    9000
+    9001
+    )";
+
+    const auto lines = clean_input(input);
+
+    constexpr int expected = 0;
+    const int actual = solution_part_one(lines);
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(SolutionTest, PartTwoExample) {
+    const auto input = R"(
+    8999
+    9000
+    9001
+    )";
+
+    const auto lines = clean_input(input);
+
+    constexpr int expected = 0;
+    const int actual = solution_part_two(lines);
+
+    EXPECT_EQ(actual, expected);
+}
