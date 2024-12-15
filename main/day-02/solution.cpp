9ba07b33bc9a6247ee8a5d1049c8317c9eee6c55
Alex Lepsa <lepsalex@gmail.com>
2024-12-15 18:21:56 -0500

    improve day 2

--- main/day-02/solution.cpp
+++ main/day-02/solution.cpp
@@ -3,106 +3,80 @@
 #include <iostream>
 #include <ranges>
 #include <sstream>
+#include <__algorithm/fold.h>
 
 namespace rng = std::ranges;
 namespace rv = std::ranges::views;
 
+struct NumbersResult {
+    long value;
+    int direction;
+    int idx;
+};
+
 std::vector<int> get_numbers_vec(const std::string &line);
 
-long evaluate_dampened_report(const std::vector<int> &numbers, int depth = 0);
+long evaluate(std::vector<int> numbers, bool retry);
 
 long solution_part_one(const std::vector<std::string> &input) {
-    long sum = 0;
-
-    for (const auto &line: input) {
-        auto numbers = get_numbers_vec(line);
-
-        int direction = 0;
-        int value{1};
-
-        for (size_t idx = 0; idx < numbers.size(); idx++) {
-            if (idx == 0) continue;
-
-            // evaluate absolute difference
-            const int abs_diff = std::abs(numbers[idx] - numbers[idx - 1]);
-            if (abs_diff == 0 || abs_diff < 1 || abs_diff > 3) {
-                value = 0;
-                break;
-            }
-
-            // get the diff direction
-            const int diff = numbers[idx] - numbers[idx - 1];
-            const int diff_direction = diff > 0 ? -1 : 1;
-
-            // the first time it's neutral, update with the direction
-            if (direction == 0) {
-                direction = diff_direction;
-                continue;
-            }
-
-            // check that the direction as not changed
-            if (diff_direction != direction) {
-                value = 0;
-                break;
-            }
-        }
-
-        sum += value;
-    }
-
-    return sum;
+    return rng::fold_left(input, 0L, [](const auto total, const auto &line) {
+        const auto numbers = get_numbers_vec(line);
+        const long result = evaluate(numbers, false);
+        return total + result;
+    });
 }
-
 
 long solution_part_two(const std::vector<std::string> &input) {
-    long sum = 0;
-
-    for (const auto &line: input) {
-        auto numbers = get_numbers_vec(line);
-        const auto line_result = evaluate_dampened_report(numbers, 0);
-        sum += line_result;
-    }
-
-    return sum;
+    return rng::fold_left(input, 0L, [](const auto total, const auto &line) {
+        const auto numbers = get_numbers_vec(line);
+        const long result = evaluate(numbers, true);
+        return total + result;
+    });
 }
 
-long evaluate_dampened_report(const std::vector<int> &numbers, const int depth) {
-    int direction = 0;
-    long result{1};
+long evaluate(std::vector<int> numbers, const bool retry) {
+    const auto result = rng::fold_left(numbers, NumbersResult(1, 0, 0),
+                                       [&numbers](auto acc, const auto number) {
+                                           if (acc.idx == 0 || acc.value == 0) {
+                                               acc.idx += 1;
+                                               return acc;
+                                           }
 
-    for (size_t idx = 0; idx < numbers.size(); idx++) {
-        if (idx == 0) continue;
+                                           const auto last_number = numbers[acc.idx - 1];
 
-        // evaluate absolute difference
-        const int abs_diff = std::abs(numbers[idx] - numbers[idx - 1]);
-        if (abs_diff == 0 || abs_diff < 1 || abs_diff > 3) {
-            result = 0;
-            break;
-        }
+                                           // evaluate absolute difference
+                                           const int abs_diff = std::abs(number - last_number);
+                                           if (abs_diff == 0 || abs_diff < 1 || abs_diff > 3) {
+                                               acc.value = 0;
+                                               return acc;
+                                           }
 
-        // get the diff direction
-        const int diff = numbers[idx] - numbers[idx - 1];
-        const int diff_direction = diff > 0 ? -1 : 1;
+                                           // get the diff direction
+                                           const int diff = number - last_number;
+                                           const int diff_direction = diff > 0 ? -1 : 1;
 
-        // the first time it's neutral, update with the direction
-        if (direction == 0) {
-            direction = diff_direction;
-            continue;
-        }
+                                           // the first time it's neutral, update with the direction
+                                           if (acc.direction == 0) {
+                                               acc.direction = diff_direction;
+                                               acc.idx += 1;
+                                               return acc;
+                                           }
 
-        // check that the direction as not changed
-        if (diff_direction != direction) {
-            result = 0;
-            break;
-        }
-    }
+                                           // check that the direction as not changed
+                                           if (acc.direction != diff_direction) {
+                                               acc.value = 0;
+                                           }
 
-    // retry false reports by removing at most one number (controlled by depth)
-    if (result == 0 && depth == 0) {
+                                           acc.idx += 1;
+                                           return acc;
+                                       });
+
+    // retry false reports by removing at most one number
+    if (result.value == 0 && retry == 1) {
         for (const auto remove_idx: rv::iota(0u, numbers.size())) {
             std::vector<int> numbers_copy = numbers;
             numbers_copy.erase(numbers_copy.begin() + remove_idx);
-            long num_removed_result = evaluate_dampened_report(numbers_copy, 1);
+            const long num_removed_result = evaluate(numbers_copy, false);
 
             if (num_removed_result == 1) {
                 return num_removed_result;
@@ -110,7 +84,7 @@ long evaluate_dampened_report(const std::vector<int> &numbers, const int depth) 
         }
     }
 
-    return result;
+    return result.value;
 }
 
 std::vector<int> get_numbers_vec(const std::string &line) {
