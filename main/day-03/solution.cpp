e090de71243ecf77e97042331049a606391fe323
Alex Lepsa <lepsalex@gmail.com>
2024-12-15 19:54:04 -0500

    day-03 part-01

--- main/day-03/solution.cpp
+++ main/day-03/solution.cpp
@@ -1,17 +1,26 @@
 #include "solution.h"
 
 #include <istream>
+#include <numeric>
 #include <ranges>
+#include <regex>
 
 namespace rng = std::ranges;
 namespace rv = std::ranges::views;
 
 long solution_part_one(const std::vector<std::string> &input) {
-    for (const auto &line: input) {
-        std::print("{}\n", line);
-    }
+    const auto text = input
+                      | rv::join
+                      | rng::to<std::string>();
 
-    return 0;
+    const std::regex regex(R"(mul\((\d{1,3}),(\d{1,3})\))");
+    std::sregex_iterator start{text.begin(), text.end(), regex};
+    const std::sregex_iterator end;
+
+    return std::reduce(start, end, 0, [](const long result, const std::smatch &matches) {
+        const long product = std::stol(matches[1].str()) * std::stol(matches[2].str());
+        return result + product;
+    });
 }
 
 long solution_part_two(const std::vector<std::string> &input) {
